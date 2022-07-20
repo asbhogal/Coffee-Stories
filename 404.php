713aa2dcea8fbf00ffec6b3936f3bb4ad11d93e1
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-07-21 00:28:57 +0100

    initial structure of theme complete. styling to commence

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php
+    get_header();
+?>
+<div id="main-header" class="main-header-content">
+    <h1>Error 404</h1>
+    < /hr>
+    <h2>This page doesn't exist...but don't worry, you can find what you were looking for below, or click the button to return back home.</h2>
+
+    <?php
+        get_search_form();
+    ?>
+
+    <a class="return-home-btn" href="/index.html"></a>
+
+</div>
+
+<?php
+    get_footer();
+?>
