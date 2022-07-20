2705af50d65f48f57b74b5f4a2141fd3e87b9950
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-07-20 16:40:19 +0100

    added site icon

--- header.php
+++ header.php
@@ -7,8 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Coffee Stories Blog">
-    <meta name="author" content="https://amansinghbhogal.com/">    
-    <link rel="shortcut icon" href="\wp-content\themes\coffeestories\assets\images\logos\Logo.png"> 
+    <meta name="author" content="https://amansinghbhogal.com/">
     <?php
         wp_head();
     ?>
