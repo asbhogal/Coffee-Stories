9264e4f1970ebc1ba70eef40dcf6b0f91f411dda
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-07-20 22:46:09 +0100

    update content-archive

---
+++ template-parts/content-archive.php
@@ -0,0 +1,37 @@
+<div class="container">
+    <div class="post">
+        <div class="media">
+            <img class="post-thumb" 
+            src="<?php
+                the_post_thumbnail_url('thumbnail');
+            ?>"
+            alt="image"> 
+            <div class="media-body">
+                <h3>
+                    <?php
+                        the_title();
+                    ?>
+                </h3>
+                <div class="meta">
+                    <span class="date"><?php the_date()."   |   "?></span>
+                    <span class="comment">
+                        <a href="#">
+                            <?php
+                                comments_number();
+                            ?>
+                        </a>
+                    </span>
+                </div>
+                <p></p>
+                    <div class="intro">
+                        <?php
+                            the_excerpt();
+                        ?>
+                    </div>
+                <p></p>
+                <a class="more-link" href="<?php the_permalink(); ?>">Read more &rarr;</a>
+            </div><!--//media-body-->
+        </div><!--//media-->
+        <p></p>
+    </div>
+</div>
